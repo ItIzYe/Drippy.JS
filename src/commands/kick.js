de05c9a3792b14022f41bbc8c56ca2b6589eb01d
ItIzYe <aderuben0@gmail.com>
2022-01-28 17:09:00 +0100

    Improved kick Command

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,3 +1,5 @@
+const { MessageEmbed, Client } = require("discord.js");
+
 module.exports = {
     name: "kick",
     description: "Dieser Command kickt einen Member!",
@@ -5,9 +7,18 @@ module.exports = {
         const member = message.mentions.users.first();
         if(member){
             const memberTarger = message.guild.members.cache.get(member.id);
-            //memberTarger.kick();
-            message.reply(`${member} wurde gekickt.`)
-            message.reply(`Grund: ${args[1]}`)
+            memberTarger.kick();
+            const exampleEmbed = new MessageEmbed()
+                .setColor('RED')
+                .setTitle('Moderation')
+                .setAuthor("JsBot")
+                .setDescription(`${member} wurde gekickt.`)
+                .addFields({name: 'Grund:', value: `${args[1]}`})
+                .setTimestamp()
+            
+            message.channel.send({ embed: exampleEmbed });
+            //message.reply(`${member} wurde gekickt.`)
+            //message.reply(`Grund: ${args[1]}`)
         }else{
             message.channel.send('Dieser Member konnte nicht gefunden werden')
         }
