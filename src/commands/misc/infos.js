c1701cfd3c7b53c7b3af539402af7c912941823f
ItIzYe <aderuben0@gmail.com>
2024-01-07 03:10:16 +0100

    moved, updated

---
+++ src/commands/misc/infos.js
@@ -0,0 +1,37 @@
+const {MessageEmbed, Client, Interaction} = require('discord.js');
+
+module.exports = {
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     */
+    name: "info",
+    description: "Get Infos about Drippy",
+
+
+    callback: async (client, interaction) => {
+
+        const infoEmbed = new MessageEmbed()
+            .setColor("#1f8a4c")
+            .setTitle("Info")
+            .setDescription("Hier ein paar Bot-Infos")
+            .addFields(
+                {name: "Name", value: "Drippy#5683", inline: false},
+                {name: "Number of servers: ", value: "Count: " + client.guilds.cache.size, inline: false},
+                {name: "Developer: ", value: "ItIzYe#7590,\nR.M.S Titanic#7956,\nCanadianAgent | Jury#9388", inline: false},
+                {name: "Version: ", value: "1.0.0 Beta", inline: false},
+                {name: "Last Update: ", value: "05.01.2022", inline: false},
+                {name: "Bug Report", value: `Bugs can be reported via bug [bug].`, inline: false}
+            )
+            .setTimestamp()
+
+
+
+        await interaction.reply({embeds: [infoEmbed]});
+
+
+    }
+}
+
+
