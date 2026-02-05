a9cac1481a12af7b9a85245bd557c903bd5a018d
ItIzYe <aderuben0@gmail.com>
2026-02-05 15:36:38 +0000

    Added levels-config

---
+++ src/commands/maintenance/config-levels.js
@@ -0,0 +1,89 @@
+const GuildConfiguration = require('../../models/GuildConfiguration');
+const {Client, Interaction, GuildChannelTypes, PermissionsBitField,} = require('discord.js')
+const mongoose = require('mongoose');
+
+const language = require("../../handlers/languages");
+
+
+module.exports = {
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     * @param {Object} param0
+     */
+    name: 'config-suggestions',
+    description: 'Configure your if you want Level-up messages sent into a specific channel',
+    options:
+        [{
+        name: 'add',
+        description: 'Add a levels channel.',
+        type: 1,
+        options: [{
+            name: 'channel',
+            description: 'The channel you want to add',
+            type: 7,
+            required: true
+            }]
+        },
+        {
+        name: 'remove',
+        description: 'Remove a levels channel.',
+        type: 1,
+        options: [{
+            name: 'channel',
+            description: 'The channel you want to remove',
+            type: 7,
+            required: true
+            }]
+        }],
+    permissionsRequired: [PermissionsBitField.Administrator],
+
+    callback: async(client, interaction) => {
+        const { guild } = interaction
+
+        let guildConfiguration = await GuildConfiguration.findOne({ guildId: interaction.guildId});
+
+        if(!guildConfiguration) {
+            guildConfiguration = new GuildConfiguration({ guildId: interaction.guildId});
+        }
+        ///const subcommand = interaction.options.get('config-suggestions')();
+
+        if(interaction.options.getSubcommand() === 'add'){
+            const channel = interaction.options.getChannel('channel');
+
+            if(guildConfiguration.levelChannelIds.includes(channel.id)){
+                await interaction.reply(`${channel} ${language(guild, 'CONFIG_SUGG_ALREADY')}`);
+                return;
+            }
+
+            guildConfiguration.levelChannelIds.push(channel.id)
+            await guildConfiguration.save();
+
+            await interaction.reply(`${channel} ${language(guild, 'CONFIG_SUGG_ADDED')}`);
+            return;
+        };
+
+        if(interaction.options.getSubcommand() === 'remove'){
+            const channel = interaction.options.getChannel('channel');
+
+            if(!guildConfiguration.levelChannelIds.includes(channel.id)){
+                await interaction.reply(`${channel} ${language(guild, 'CONFIG_SUGG_NOT')}`);
+                return;
+            }
+
+            guildConfiguration.suggestionChannelIds = guildConfiguration.levelChannelIds.filter((id) =>id !== channel.id)
+            await guildConfiguration.save();
+
+            await interaction.reply(`${channel} ${language(guild, 'CONFIG_SUGG_REM')}`);
+            return;
+        }
+    }
+
+
+
+
+   /** data: {},
+    run: () => {},
+    options: {}**/
+}
