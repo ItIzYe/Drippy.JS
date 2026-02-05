519b4e97fdc6082228a65fd8bc2fb727bced0278
ItIzYe <aderuben0@gmail.com>
2026-02-05 16:10:07 +0000

    added Level configs

--- src/models/GuildConfiguration.js
+++ src/models/GuildConfiguration.js
@@ -16,6 +16,10 @@ const guildConfigurationSchema = new Schema({
     announcementChannelIds: {
         type: [String],
         default: []
+    },
+    levelChannelIds: {
+        type: [String],
+        default: []
     }
 });
 
