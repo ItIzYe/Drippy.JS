e9479203a4d301a88d1138be0d3ff1f679fc3df7
ItIzYe <aderuben0@gmail.com>
2026-04-20 02:47:30 +0200

    Update announcement.js

--- src/commands/teammaintenance/announcement.js
+++ src/commands/teammaintenance/announcement.js
@@ -40,43 +40,47 @@ module.exports = {
     ],
     testOnly: true,
 
-    callback: async (client, interaction) => {
-        //await interaction.deferReply();
+   callback: async (client, interaction) => {
+    // 1. Defer reply, da das Senden an viele Guilds Zeit benötigt
+    await interaction.deferReply({ ephemeral: true });
 
-        const announcement_message = interaction.options.get('message').value;
-        const announcement_type = interaction.options.get('message-type').value;
+    const announcement_message = interaction.options.getString('message');
+    const announcement_type = interaction.options.getString('message-type');
 
-        const embed = new EmbedBuilder()
-            .setColor('Yellow')
-            .setTitle('ANNOUNCEMENT - ANKÜNDIGUNG')
-            .setDescription('The following Announcement will be broadcasted in english - Die folgende Ankündigung wird auf Englisch erscheinen')
-            .setFields(
-                {name: `${announcement_type}`, value: `${announcement_message}`, inline: true}
-            )
+    const embed = new EmbedBuilder()
+        .setColor('Yellow')
+        .setTitle('ANNOUNCEMENT - ANKÜNDIGUNG')
+        .setDescription('The following Announcement will be broadcasted in English.')
+        .addFields(
+            { name: `${announcement_type}`, value: `${announcement_message}`, inline: true }
+        );
 
+    // 2. Benutze eine for...of Schleife für asynchrone Abläufe
+    const guilds = client.guilds.cache.values();
 
+    for (const guild of guilds) {
+        try {
+            // 3. WICHTIG: await benutzen!
+            let guildConfiguration = await GuildConfiguration.findOne({ guildId: guild.id });
+            
+            let targetChannel;
 
-        client.guilds.cache.forEach( guild =>{
-            //console.log(guild.id)
-
-            let guildConfiguration = GuildConfiguration.findOne({ guildId: guild.id});
-            const announcement_channel = client.channels.cache.get(`${guild.systemChannelId}`);
-
-            //console.log(announcement_channel)
-            try {
-                if (guildConfiguration.announcementChannelIds && guildConfiguration.announcementChannelIds.length > 0) {
-                    const messageChannel = client.channels.cache.get(guildConfiguration.announcementChannelIds[0])
-                    //console.log(guildConfiguration.announcementChannelIds)
-                    messageChannel.send({embeds: [embed]});
-                } else {
-                    announcement_channel.send({embeds: [embed]});
-                }
-            } catch (error) {
-                console.log(`There was an error when announcing: ${error}`);
+            if (guildConfiguration?.announcementChannelIds?.length > 0) {
+                targetChannel = await client.channels.fetch(guildConfiguration.announcementChannelIds[0]).catch(() => null);
+            } else {
+                // Fallback auf System Channel
+                targetChannel = guild.systemChannel;
             }
-        })
 
-        interaction.reply("Announcement has been sent")
+            if (targetChannel) {
+                await targetChannel.send({ embeds: [embed] }).catch(err => console.log(`Senden fehlgeschlagen für ${guild.name}`));
+            }
+        } catch (error) {
+            console.error(`Fehler bei Guild ${guild.id}: ${error}`);
+        }
+    }
 
-    },
+    // 4. Abschlussmeldung
+    await interaction.editReply("Announcement has been sent to all reachable guilds.");
+},
 };
