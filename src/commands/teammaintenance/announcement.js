c32e8b8ba8adc5d96f45026800daf922fbc97ce4
ItIzYe <aderuben0@gmail.com>
2026-04-20 03:06:03 +0200

    Update announcement.js

--- src/commands/teammaintenance/announcement.js
+++ src/commands/teammaintenance/announcement.js
@@ -1,86 +1,47 @@
-const GuildConfiguration = require('../../models/GuildConfiguration');
-const ImageConfiguration = require('../../models/Images');
-const {
-    Client,
-    Interaction,
-    ApplicationCommandOptionType,
-    PermissionFlagsBits,
-    EmbedBuilder
+const { 
+    Client, 
+    Interaction, 
+    ModalBuilder, 
+    TextInputBuilder, 
+    TextInputStyle, 
+    ActionRowBuilder, 
+    PermissionFlagsBits 
 } = require('discord.js');
-const language = require("../../handlers/languages");
-
 
 module.exports = {
-    /**
-     *
-     * @param {Client} client
-     * @param {Interaction} interaction
-     * @param {Object} 0param
-     */
     name: 'announcement',
-    description: 'Create an Bot announcement',
+    description: 'Öffnet ein Fenster für eine Bot-Ankündigung',
     devOnly: true,
-    testOnly: true,
-    // options: Object[],
-    // deleted: Boolean,
-    options: [
-        {
-            name: 'message',
-            description: 'Enter the message.',
-            type: 3,
-            required: true,
-        },
-        {
-            name: 'message-type',
-            description: 'Enter the message.',
-            type: 3,
-            required: true,
-        },
 
-    ],
-    testOnly: true,
+    callback: async (client, interaction) => {
+        // Modal erstellen
+        const modal = new ModalBuilder()
+            .setCustomId(`announcement_modal_${interaction.user.id}`)
+            .setTitle('Neue Bot-Ankündigung');
 
-   callback: async (client, interaction) => {
-    // 1. Defer reply, da das Senden an viele Guilds Zeit benötigt
-    await interaction.deferReply({ ephemeral: true });
+        // Eingabefeld für den Titel/Typ
+        const typeInput = new TextInputBuilder()
+            .setCustomId('announcement_type')
+            .setLabel("Was für eine Art von Ankündigung?")
+            .setPlaceholder("z.B. Wartungsarbeiten, Update, Info...")
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
 
-    const announcement_message = interaction.options.getString('message');
-    const announcement_type = interaction.options.getString('message-type');
+        // Eingabefeld für die Nachricht
+        const messageInput = new TextInputBuilder()
+            .setCustomId('announcement_message')
+            .setLabel("Deine Nachricht")
+            .setPlaceholder("Schreibe hier den Inhalt der Ankündigung...")
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true);
 
-    const embed = new EmbedBuilder()
-        .setColor('Yellow')
-        .setTitle('ANNOUNCEMENT - ANKÜNDIGUNG')
-        .setDescription('The following Announcement will be broadcasted in English.')
-        .addFields(
-            { name: `${announcement_type}`, value: `${announcement_message}`, inline: true }
-        );
+        // Zeilen zum Modal hinzufügen
+        const firstActionRow = new ActionRowBuilder().addComponents(typeInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(messageInput);
 
-    // 2. Benutze eine for...of Schleife für asynchrone Abläufe
-    const guilds = client.guilds.cache.values();
+        modal.addComponents(firstActionRow, secondActionRow);
 
-    for (const guild of guilds) {
-        try {
-            // 3. WICHTIG: await benutzen!
-            let guildConfiguration = await GuildConfiguration.findOne({ guildId: guild.id });
-            
-            let targetChannel;
-
-            if (guildConfiguration?.announcementChannelIds?.length > 0) {
-                targetChannel = await client.channels.fetch(guildConfiguration.announcementChannelIds[0]).catch(() => null);
-            } else {
-                // Fallback auf System Channel
-                targetChannel = guild.systemChannel;
-            }
-
-            if (targetChannel) {
-                await targetChannel.send({ embeds: [embed] }).catch(err => console.log(`Senden fehlgeschlagen für ${guild.name}`));
-            }
-        } catch (error) {
-            console.error(`Fehler bei Guild ${guild.id}: ${error}`);
-        }
-    }
-
-    // 4. Abschlussmeldung
-    await interaction.editReply("Announcement has been sent to all reachable guilds.");
-},
+        // Modal anzeigen
+        await interaction.showModal(modal);
+    },
 };
