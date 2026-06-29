89ca8c55d7a11fbf1fc09395e3364467e594e072
ItIzYe <aderuben0@gmail.com>
2026-06-29 17:10:32 +0200

    added a new captcha method.

---
+++ src/commands/maintenance/captchaeu.js
@@ -0,0 +1,111 @@
+const {EmbedBuilder, Permissions, Client, Interaction, MessageFlags} = require('discord.js');
+const mongoose = require('mongoose');
+const BugConfig = require('../../models/BugConfig');
+const GuildConfiguration = require("../../models/GuildConfiguration");
+const VerifyConfig = require('../../models/Verify');
+
+const language = require('../../handlers/languages')
+
+module.exports = {
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     * @param {Object} param0
+     */
+    name: "captcha-eu",
+    description: "Captcha (EU and Schengen Area)",
+    //devOnly: true,
+    //testOnly: true,
+    options: [{
+        name: "id",
+        description: "Please enter your ID",
+        type: 3,
+        required: true
+    }],
+    //deleted: Boolean,
+
+    callback: async (client, interaction) => {
+        //console.log(interaction)
+        await interaction.deferReply();
+
+        const { guild, member, user, customId } = interaction;
+
+        const config = await VerifyConfig.findOne({ guildId: guild.id });
+            
+            if (!config || !config.roleId) {
+                return await interaction.reply({ 
+                    content: '❌ Fehler: Es wurde noch keine Rolle konfiguriert.', 
+                    flags: [MessageFlags.Ephemeral] 
+                });
+            }
+        
+            const role = guild.roles.cache.get(config.roleId);
+            if (!role) {
+                return await interaction.reply({ 
+                    content: '❌ Die konfigurierte Rolle existiert nicht mehr.', 
+                    flags: [MessageFlags.Ephemeral] 
+                });
+            }
+
+
+
+        const ziffer = interaction.options.get('id').value;
+
+        let j = 0;
+        let i = 0;
+
+        for(let n = 0; n < 6; n++){
+          if(i === 0){
+            j = j + (Number(ziffer[n]) * 7);
+            i++;
+            console.log(j);
+            console.log(i);
+          } else if(i === 1){
+            j = j + (Number(ziffer[n]) * 3);
+            i++;
+            console.log(j);
+            console.log(i);
+          }else if(i === 2){
+            j = j + (Number(ziffer[n]) * 1);
+            i = 0;
+            console.log(j);
+            console.log(i);
+          }
+        }
+
+        const d = new Date();
+
+        const yy = String(d.getFullYear()).slice(-2);
+        const mm = String(d.getMonth() + 1).padStart(2, '0');
+        const dd = String(d.getDate()).padStart(2, '0');
+        const datumString = yy + mm + dd;
+        const datumInteger = Number(datumString);
+        console.log(datumInteger);
+
+        const alter = String(ziffer.slice(0, 6));
+        const check = datumInteger - Number(alter);
+        if(check >= 160000){
+          try {
+                  await member.roles.add(role);
+                  await interaction.editReply({ 
+                      content: `✅ Du wurdest erfolgreich verifiziert und hast die Rolle **${role.name}** erhalten!`, 
+                      flags: [MessageFlags.Ephemeral] 
+                  });
+              } catch (error) {
+                  console.error(error);
+                  await interaction.editReply({ 
+                      content: '❌ Mir fehlen die Berechtigungen für diese Rolle.', 
+                      flags: [MessageFlags.Ephemeral] 
+                  });
+              }
+        }else{
+          return await interaction.editReply({ 
+                      content: '⚠️ Dein Account ist noch zu neu für diesen Server.', 
+                      flags: [MessageFlags.Ephemeral] 
+                  });
+        }
+
+
+      }
+};
---
+++ src/commands/moderation/setup-captcha-eu.js
@@ -0,0 +1,70 @@
+const { 
+    EmbedBuilder, 
+    ActionRowBuilder, 
+    ButtonBuilder, 
+    ButtonStyle, 
+    PermissionsBitField, 
+    ApplicationCommandOptionType,
+    MessageFlags,
+    AttachmentBuilder
+} = require('discord.js');
+const VerifyConfig = require('../../models/Verify');
+const path = require('path');
+
+module.exports = {
+    name: 'setup-captcha-eu',
+    description: 'Richtet die Verifizierung mit einer spezifischen Rolle ein',
+    permissionsRequired: [PermissionsBitField.Flags.Administrator],
+    options: [
+        {
+            name: 'rolle',
+            description: 'Die Rolle, die nach der Verifizierung vergeben wird',
+            type: ApplicationCommandOptionType.Role,
+            required: true,
+        },
+        {
+            name: 'channel',
+            description: 'Der Kanal, in den die Appeals gesendet werden sollen.',
+            type: ApplicationCommandOptionType.Channel,
+            required: true,
+        },
+    ],
+
+    callback: async (client, interaction) => {
+        const targetRole = interaction.options.getRole('rolle');
+        const kanal = interaction.options.getChannel('channel');
+
+        await VerifyConfig.findOneAndUpdate(
+            { guildId: interaction.guildId },
+            { roleId: targetRole.id },
+            { upsert: true }
+        );
+
+        const dateiPfad = path.join(__dirname, '..', '..', 'img', 'perso.jpg'); 
+        const bildAttachment = new AttachmentBuilder(dateiPfad, { name: 'perso.jpg' });
+
+        const embed = new EmbedBuilder()
+            .setTitle('🛡️ Altersbestätigung')
+            .setImage('attachment://perso.jpg')
+            .setDescription(
+                `Für den Zugriff auf die freigegebenen Bereiche dieses Servers ist eine Bestätigung des Mindestalters (16+) erforderlich.\n\n**Ablauf der Verifizierung:**\n`
+            )
+            .addFields(
+              {name: "1.", value: `Wechsel in den Kanal ${kanal}.`, inline: false},
+              {name: "2.", value: "Führe dort den Befehl \`/captcha-eu\` aus.", inline:false},
+              {name: "3.", value: "Gib die **7-stellige Zahlenfolge** von der Rückseite seines Ausweises ein (Siehe Grafik).", inline:false},
+              {name: "**DATENSCHUTZ- UND SICHERHEITSHINWEISE:", value: `• Der Befehl wird als private Interaktion (*ephemeral*) ausgeführt. Ihre Eingabe ist für andere Servermitglieder nicht einsehbar.\n` +
+        `• Die Daten werden ausschließlich flüchtig im Arbeitsspeicher verarbeitet, um den mathematischen Prüfalghorithmus (ICAO-9303-Standard) auszuführen. Eine Speicherung oder Protokollierung der Ziffern findet zu keinem Zeitpunkt statt.`}
+            )
+            .setColor('Blue');
+
+        
+
+        await interaction.channel.send({ embeds: [embed], files: [bildAttachment], });
+
+        await interaction.reply({ 
+            content: `Verifizierung eingerichtet! Ziel-Rolle: **${targetRole.name}**`, 
+            flags: [MessageFlags.Ephemeral] 
+        });
+    }
+};
